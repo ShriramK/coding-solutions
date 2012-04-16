bffbd401909635737cc8a88231e06a6703969967
Shriram Kunchanapalli <kshriram18@gmail.com>
2012-04-16 16:17:10 +0530

    Added code in solution to use mapping from original to Google and output

--- GCJ/P0/Main.py
+++ GCJ/P0/Main.py
@@ -1,14 +1,19 @@
 def spkint():
 	inputFile = open('A-small-practice.in', 'r')
-	outputFile = open('A-small-practice.out', 'w')
+	#outputFile = open('A-small-practice.out', 'w')
+	outputFile = open('A-small-practice2.out', 'w')
 	size = inputFile.readline()
-	li = ['y','h','e','s','o','c','v','x','d','u','i','g','l','b','k','r','z','t','n','w','j','p','f','m','a','q']
+	#li = ['y','h','e','s','o','c','v','x','d','u','i','g','l','b','k','r','z','t','n','w','j','p','f','m','a','q']
+	liA = ['y','n','f','i','c','w','l','b','k','u','o','m','x','s','e','v','z','p','d','r','j','g','t',
+'h','a','q']
 	val = 0
+	addNum = ord('a')
 	for line in inputFile:
 		outputFile.write("Case #%s: " % (val+1))
 		for ch in line:
-			if ch.isalpha():
-				outputFile.write(li[ord(ch)-ord('a')])
+			if ch.isalpha():#check encoding
+				outputFile.write(chr(liA.index(ch)+ addNum))
+				#outputFile.write(li[ord(ch)-ord('a')])
 			elif ch == ' ':
 				outputFile.write(ch)
 		outputFile.write('\n')#could it be outputFile.write()
