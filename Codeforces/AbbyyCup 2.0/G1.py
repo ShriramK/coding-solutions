ed6555f3f76c7f82fe353d6153fa83eb92d1cff0
Shriram Kunchanapalli <kshriram18@gmail.com>
2012-04-22 10:19:32 +0530

    Added solution for Problem G1. Fibonacci Strings

---
+++ Codeforces/AbbyyCup 2.0/G1.py
@@ -0,0 +1,18 @@
+import sys
+val = raw_input()
+li = val.split()
+
+str1 = "a"
+str2 = "b"
+cnt = 2 
+while cnt < int(li[0]):
+    temp = str2
+    str2 += str1
+    str1 = temp
+    cnt += 1
+print 'str ', str2
+for i in range(int(li[1])):
+    testStr = raw_input()
+    print 'testStr ', testStr
+    if testStr in str2:
+        print str2.count(testStr)
