e3564a282d91c7754034e23b894d5e845498c8ec
Shriram Kunchanapalli <kshriram18@gmail.com>
2012-05-06 08:37:40 +0530

    Revising the solution for G1.py and adding the testcases for the problem

--- Codeforces/ABBYY Cup 2.0/G1.py
+++ Codeforces/ABBYY Cup 2.0/G1.py
@@ -1,20 +1,32 @@
 #http://codeforces.com/contest/177/problem/G1
 #Fibonacci Numbers
-import sys
 val = raw_input()
 li = val.split()
 
 str1 = "a"
 str2 = "b"
-cnt = 2 
-while cnt < int(li[0]):
-    temp = str2
-    str2 += str1
-    str1 = temp
-    cnt += 1
-print 'str ', str2
+if int(li[0]) == 1:
+	str2 = "a"
+elif int(li[0]) == 2:
+	str2 = "b"
+else:
+	cnt = 2 
+	while cnt < int(li[0]):
+		temp = str2
+		str2 += str1
+		str1 = temp
+		cnt += 1
+#print 'str ', str2
+val = (1000000007*(10**9+7))
 for i in range(int(li[1])):
     testStr = raw_input()
-    print 'testStr ', testStr
+    #print 'testStr ', testStr
     if testStr in str2:
-        print str2.count(testStr)
+		count = 0
+		for j in range(0, len(str2) - len(testStr) + 1):
+			if str2[j:len(testStr)+j] == testStr:
+				count += 1
+				count %= val
+		print count % val
+    else:
+		print 0
