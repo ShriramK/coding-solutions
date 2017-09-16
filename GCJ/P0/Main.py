137264862548028fe2bcd5a0b7cbf09688681841
Shriram Kunchanapalli <kshriram18@gmail.com>
2017-09-16 09:32:31 +0530

    Modifies code to adhere to pep8

--- GCJ/P0/Main.py
+++ GCJ/P0/Main.py
@@ -1,24 +1,24 @@
-def spkint():
-	inputFile = open('A-small-practice.in', 'r')
-	#outputFile = open('A-small-practice.out', 'w')
-	outputFile = open('A-small-practice2.out', 'w')
-	size = inputFile.readline()
-	#li = ['y','h','e','s','o','c','v','x','d','u','i','g','l','b','k','r','z','t','n','w','j','p','f','m','a','q']
-	liA = ['y','n','f','i','c','w','l','b','k','u','o','m','x','s','e','v','z','p','d','r','j','g','t',
-'h','a','q']
-	val = 0
-	addNum = ord('a')
-	for line in inputFile:
-		outputFile.write("Case #%s: " % (val+1))
+def translate_googlerese_text():
+	input_file = open('A-small-practice.in', 'r')
+	# output_file = open('A-small-practice.out', 'w')
+	output_file = open('A-small-practice2.out', 'w')
+	num_of_lines = input_file.readline()
+	# li = ['y','h','e','s','o','c','v','x','d','u','i','g','l','b','k','r','z','t','n','w',
+	# 'j','p','f','m','a','q']
+	list_of_chars = ['y','n','f','i','c','w','l','b','k','u','o','m','x','s','e','v','z']
+	list_of_chars.extend(['p','d','r','j','g','t','h','a','q'])
+	case_num = 0
+	for line in input_file:
+		output_file.write("Case #%s: " % (case_num + 1))
 		for ch in line:
-			if ch.isalpha():#check encoding
-				outputFile.write(chr(liA.index(ch)+ addNum))
-				#outputFile.write(li[ord(ch)-ord('a')])
+			if ch.isalpha(): # check encoding
+				output_file.write(chr(list_of_chars.index(ch) + ord('a')))
+				# output_file.write(list_of_chars[ord(ch) - ord('a')])
 			elif ch == ' ':
-				outputFile.write(ch)
-		outputFile.write('\n')#could it be outputFile.write()
-		val += 1
-	outputFile.close()
-	
+				output_file.write(ch)
+		output_file.write('\n') # could it be output_file.write()
+		case_num += 1
+	output_file.close()
+
 if __name__ == '__main__':
-	spkint()
+	translate_googlerese_text()
