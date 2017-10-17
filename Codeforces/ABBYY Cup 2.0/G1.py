d560be771ef9a36fd05ccf5428f7c0ad914a0b11
Shriram Kunchanapalli <kshriram18@gmail.com>
2017-10-17 19:02:23 +0530

    Modifies code to adhere to pep8
    http://docs.python-guide.org/en/latest/writing/style/#general-concepts

--- Codeforces/ABBYY Cup 2.0/G1.py
+++ Codeforces/ABBYY Cup 2.0/G1.py
@@ -10,38 +10,38 @@ first_fib_str = "a"
 second_fib_str = "b"
 fib_string_index = int(operator.getitem(input_params, 0))
 if fib_string_index == 1:
-	second_fib_str = "a"
+    second_fib_str = "a"
 elif fib_string_index == 2:
-	second_fib_str = "b"
+    second_fib_str = "b"
 else:
-	cnt = 2
-	while cnt < fib_string_index:
-		temp = second_fib_str
-		second_fib_str += first_fib_str
-		first_fib_str = temp
-		cnt += 1
+    cnt = 2
+    while cnt < fib_string_index:
+        temp = second_fib_str
+        second_fib_str += first_fib_str
+        first_fib_str = temp
+        cnt += 1
 print 'str ', second_fib_str
 val = 1000000007 * (10**9+7)
 
 num_of_queries = int(input_params[1])
 for i in range(num_of_queries):
     test_str = raw_input()
-    #print 'test_str ', test_str
+    # print 'test_str ', test_str
     first_char = operator.getitem(test_str, 0)
     list_of_pos = []
     for pos in range(len(second_fib_str)):
-    	fib_char = operator.getitem(second_fib_str, pos)
+        fib_char = operator.getitem(second_fib_str, pos)
         if fib_char == first_char:
             list_of_pos.append(pos)
     if test_str in second_fib_str:
         count = 0
-        for j in list_of_pos:		
-		# for j in range(0, len(second_fib_str) - len(test_str) + 1):
-			# if second_fib_str[j:j + 1] == test_str[:1] and \
-			# second_fib_str[j:len(test_str) + j] == test_str:
-			if second_fib_str[j : len(test_str) + j] == test_str:
-				count += 1
-				count %= val
+        for j in list_of_pos:
+            # for j in range(0, len(second_fib_str) - len(test_str) + 1):
+            # if second_fib_str[j:j + 1] == test_str[:1] and \
+            # second_fib_str[j:len(test_str) + j] == test_str:
+            if second_fib_str[j: len(test_str) + j] == test_str:
+                count += 1
+                count %= val
         print count
     else:
-		print 0
+        print 0
--- SPOJ/ONP.py
+++ SPOJ/ONP.py
@@ -1,41 +1,44 @@
 # http://www.spoj.pl/problems/ONP/
 import sys
 
+
 def transform_expression(algebraic_expression):
-	output = ""
-	li = []
-	for i in algebraic_expression:
-		if str.isalpha(i):
-			output += i
-		elif i == '(':
-			li.append(i)
-		elif i == ')':
-			temp = li.pop()
-			while temp != '(':
-				output += temp
-				temp = li.pop()
-		else:
-			if li[len(li) - 1] == '(' or \
-				det_priority(i) < det_priority(li[len(li) - 1]):
-				li.append(i)
-			else:
-				output += li.pop()
-				li.append(i)			
-	return output
+    output = ""
+    li = []
+    for i in algebraic_expression:
+        if str.isalpha(i):
+            output += i
+        elif i == '(':
+            li.append(i)
+        elif i == ')':
+            temp = li.pop()
+            while temp != '(':
+                output += temp
+                temp = li.pop()
+        else:
+            if li[len(li) - 1] == '(' or \
+               det_priority(i) < det_priority(li[len(li) - 1]):
+                li.append(i)
+            else:
+                output += li.pop()
+                li.append(i)
+    return output
+
 
 def det_priority(operator_param):
-	val = -1
-	operator_values = {
-		'+': 0,
-		'-': 1,
-		'*': 2,
-		'/': 3,
-		'^': 4,
-		'(': 5,
-		}
-	val = operator_values[operator_param]
-	return val
+    val = -1
+    operator_values = {
+        '+': 0,
+        '-': 1,
+        '*': 2,
+        '/': 3,
+        '^': 4,
+        '(': 5,
+        }
+    val = operator_values[operator_param]
+    return val
+
 
 num_of_expressions = int(raw_input())
 for i in range(num_of_expressions):
-	print transform_expression(raw_input())
+    print transform_expression(raw_input())
